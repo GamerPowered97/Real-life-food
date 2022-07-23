29efd6fb6fc82f7fe5479232dea14f7ce7413961
Michael <toschimichael22@gmail.com>
2022-07-23 02:07:44 +0200

    Mass update

--- src/main/java/net/gamerpowered97/foodmod/item/ModItems.java
+++ src/main/java/net/gamerpowered97/foodmod/item/ModItems.java
@@ -3,7 +3,6 @@ package net.gamerpowered97.foodmod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.gamerpowered97.foodmod.FoodMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -11,8 +10,18 @@ public class ModItems {
     public static final Item PIZZA_SLICE = registerItem("pizza_slice",
             new Item(new FabricItemSettings().group(ModItemGroup.FOOD_MOD).food(ModFoodComponents.PIZZA_SLICE)));
 
-    public static final Item DONUT = registerItem("donut",
-            new Item(new FabricItemSettings().group(ModItemGroup.FOOD_MOD)));
+    public static final Item STRAWBERRY_DONUT = registerItem("strawberry_donut",
+            new Item(new FabricItemSettings().group(ModItemGroup.FOOD_MOD).food(ModFoodComponents.STRAWBERRY_DONUT)));
+
+    public static final Item DOUGH = registerItem("dough",
+            new Item(new FabricItemSettings().group(ModItemGroup.FOOD_MOD).food(ModFoodComponents.DOUGH)));
+
+    public static final Item PLAIN_DONUT = registerItem("plain_donut",
+            new Item(new FabricItemSettings().group(ModItemGroup.FOOD_MOD).food(ModFoodComponents.PLAIN_DONUT)));
+
+    public static final Item FROSTING = registerItem("frosting",
+            new Item(new FabricItemSettings().group(ModItemGroup.FOOD_MOD).food(ModFoodComponents.FROSTING)));
+
 
 
 
