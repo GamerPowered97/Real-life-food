b9dc57ff5ae2e59963e95d8b92c785d5dfe628df
Michael <toschimichael22@gmail.com>
2022-07-25 18:39:39 +0200

    Made the tomato vine drop the perfect amount of seeds

--- src/main/java/net/gamerpowered97/foodmod/item/ModFoodComponents.java
+++ src/main/java/net/gamerpowered97/foodmod/item/ModFoodComponents.java
@@ -9,9 +9,9 @@ public class ModFoodComponents {
     public static final FoodComponent STRAWBERRY_DONUT = new FoodComponent.Builder().hunger(6).saturationModifier(1.2f).statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 200, 0), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 600, 0), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 400, 0), 0.3f).build();
     public static final FoodComponent PLAIN_DONUT = new FoodComponent.Builder().hunger(5).saturationModifier(0.3f).build();
     public static final FoodComponent FROSTING = new FoodComponent.Builder().hunger(1).saturationModifier(0f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 100, 1), 1.0f).alwaysEdible().build();
-    public static final FoodComponent DOUGH = new FoodComponent.Builder().hunger(2).saturationModifier(0f).statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 200, 1), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 1), 1.0f).build();
+    public static final FoodComponent DOUGH = new FoodComponent.Builder().hunger(2).saturationModifier(0f).statusEffect(new StatusEffectInstance(StatusEffects.POISON, 200, 0), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 200, 1), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 1), 1.0f).build();
     public static final FoodComponent TOMATO = new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).build();
     public static final FoodComponent CHEESE = new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).build();
-    public static final FoodComponent WHEAT_DOUGH = new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 200, 1), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 1), 1.0f).build();
+    public static final FoodComponent WHEAT_DOUGH = new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).statusEffect(new StatusEffectInstance(StatusEffects.POISON, 200, 0), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 200, 1), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 1), 1.0f).build();
     public static final FoodComponent PLAIN_PIZZA_SLICE = new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build();
 }
