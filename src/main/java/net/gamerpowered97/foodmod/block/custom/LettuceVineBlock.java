419a04990f6e68be094ae5999958b4d219611cb7
Michael <toschimichael22@gmail.com>
2022-10-09 19:21:19 +0200

    Fixed the lettuce vine not growing the right age and fixed its texture not loading

--- src/main/java/net/gamerpowered97/foodmod/block/custom/LettuceVineBlock.java
+++ src/main/java/net/gamerpowered97/foodmod/block/custom/LettuceVineBlock.java
@@ -1,14 +1,34 @@
 package net.gamerpowered97.foodmod.block.custom;
 
 import net.gamerpowered97.foodmod.item.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
 
 public class LettuceVineBlock extends CropBlock {
+    public static final IntProperty AGE = IntProperty.of("age", 0, 3);
+
     public LettuceVineBlock(Settings settings) {
         super(settings);
     }
 
+    @Override
+    public int getMaxAge() {
+        return 3;
+    }
+
+    @Override
+    public IntProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
+    }
     @Override
     protected ItemConvertible getSeedsItem() {
         return ModItems.LETTUCE_SEEDS;
